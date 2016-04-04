c55ec13c837d2b90705a888b014216f86ecf4512
Raymond Seger <raymondseger@live.com>
2016-04-04 10:14:11 +0700

    use marked library, but there's a problem!
    it displays as <p>Content</p> instead of displaying the HTML

--- myReactJS/tutorial4.js
+++ myReactJS/tutorial4.js
@@ -1,17 +1,19 @@
 // tutorial4.js
 var Comment = React.createClass({
+  
   render: function() {
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        {marked(this.props.children.toString())}
       </div>
     );
   }
 });
 
+// render can only inserts 1 object, that is why we use <div className="commentList">
 ReactDOM.render(
   <div className="commentList">
     <Comment author="raymond">raymond content</Comment>
@@ -19,4 +21,13 @@ ReactDOM.render(
     <Comment author="Jordan Walke">This is *another* comment</Comment>
   </div>,
   document.getElementById('content')
+);
+
+ReactDOM.render(
+  <div className="commentList">
+    <Comment author="raymond">raymond content</Comment>
+    <Comment author="Pete Hunt">This is one comment</Comment>
+    <Comment author="Jordan Walke">This is *another* comment</Comment>
+  </div>,
+  document.getElementById('content2')
 );
